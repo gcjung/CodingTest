14f0e1e7fe0d745740560e1d45e270a495da5eca
gcjung <101786156+gcjung@users.noreply.github.com>
2023-08-01 16:55:54 +0900

    [level 3] Title: 야근 지수, Time: 17.36 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+long long solution(int n, vector<int> works) {
+   long long answer = 0;
+    priority_queue<int,vector<int>> pq;
+    for(int i = 0; i < works.size(); i++){
+        pq.push(works[i]);
+    }
+    for(int i = 0; i < n; i++){
+        if(pq.top() > 0){
+            long long t = pq.top() - 1;
+            pq.pop();
+            pq.push(t);
+        }
+    }
+    while(!pq.empty()){
+        long long t = pq.top();
+        pq.pop();
+        answer += t *t;
+    }
+    return answer;
+}
